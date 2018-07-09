7d958ff9c0d3f3bcbf66a746d6eee8ca43389634
Ken Jannette <KenJannette@kens-mbp.attlocal.net>
2018-07-08 20:37:40 -0400

    Edited server.js for production/deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+const port = process.env.PORT || 8080;
 
 app.use('/static', express.static('public'))
 
@@ -8,4 +9,4 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(8080);
+app.listen(port);
